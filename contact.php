ef32cd3ac0ba483b4e320dcb20b196e7577aa5be
Katherine Hambley <superdesigngirl@mac.com>
2014-07-31 14:13:31 -0500

    used POST method and extract function to get form data

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
 <body>
 <?php
 
-echo "<h3>Thank you!</h3>";
+/*echo "<h3>Thank you!</h3>";
 echo "Here is a copy of your request:<br/><br/>";
 
 echo "Name: ".$_POST['name']."<br/>";
@@ -18,8 +18,41 @@ echo "Subject: ".$_POST['subject']."<br/>";
 echo "Message: ".$_POST['message']."<br/>";
 echo "How you heard about us: ".$_POST['found']."<br/>";
 echo "Update you about our products: ".$_POST['update1']."<br/>";
-echo "Update you about partners' products: ".$_POST['update2']."<br/>";
+echo "Update you about partners' products: ".$_POST['update2']."<br/>";*/
 
+/*extract($_POST, EXTR_PREFIX_SAME, "post");
+
+echo "<h3>Thank you!</h3>";
+echo "Here is a copy of your request:<br/><br/>";
+
+echo "Name: ".$name."<br/>";
+echo "Email: ".$email."<br/>";
+echo "Type of Request: ".$whoami."<br/>";
+echo "Subject: ".$subject."<br/>";
+echo "Message: ".$message."<br/>";
+echo "How you heard about us: ".$found."<br/>";
+echo "Update you about our products: ".$update1."<br/>";
+echo "Update you about partners' products: ".$update2."<br/>";*/
+
+extract($_POST, EXTR_PREFIX_SAME, "post");
+
+echo "<h3>Thank you!</h3>";
+echo "Here is a copy of your request:<br/><br/>";
+
+echo "Name: ".$name."<br/>";
+echo "Email: ".$email."<br/>";
+echo "Type of Request: ".$whoami."<br/>";
+echo "Subject: ".$subject."<br/>";
+echo "Message: ".$message."<br/>";
+echo "How you heard about us: ".$found."<br/>";
+
+
+for ($i = 1; $i <= 2; $i++) {
+   $element_name = "update".$i;
+   echo $element_name.": ";
+   echo $$element_name;
+   echo "<br/>";
+}
 ?>
 </body>
 </html>
